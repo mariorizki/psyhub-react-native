e82cc506e982cfd218eb9854b782fb5430cdd39c
Mario Rizki <mariorizki07@gmail.com>
2019-09-17 20:53:16 +0700

    implement styled component

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is Dema-F mobile app, Psyhub!!</Text>
-    </View>
+    <Container>
+      <TitleBar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+        <Title>Welcome back,</Title>
+        <Name>Mario</Name>
+      </TitleBar>
+      <Subtitle>Featured Post</Subtitle>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
